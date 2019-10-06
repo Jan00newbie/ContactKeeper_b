97d84279e0ad61ae76069d1bc952b9fe6e9f6430
jan00newbie <positiveKiddo@gmail.com>
2019-10-06 13:02:05 +0200

    Login & sing in ( + jwt token)complete

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../Models/User')
+const secret = require('../config/default.json').secret
+
 const router = express.Router()
 
 
@@ -8,8 +14,36 @@ const router = express.Router()
  * @returns auth token
  * @access private
  */
-router.get('/', (req, res) => {
-    res.send("auth")
+router.post('/',[
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').not().isEmpty(),
+], async (req, res) => {
+    const errors = validationResult(req)
+    
+    if(!errors.isEmpty()){
+        return res.status(400).send(errors.array())
+    }
+
+    const { email, password } = req.body
+
+    const foundUser = await User.findOne({email})
+
+    const wrongDataEnteredMessage = 'The password or email that have been entered is incorrect.'
+
+    if(!foundUser){
+        return res.status(400).send({err: wrongDataEnteredMessage})
+    }
+    const isCorrectPassword = await bcrypt.compare(password, foundUser.password);
+    
+    if(!isCorrectPassword){
+        return res.status(400).send({err: wrongDataEnteredMessage})
+    }
+
+    const token = jwt.sign({id: foundUser._id}, secret, {
+        expiresIn: '1h'
+    })
+
+    return res.status(200).send({token})
 })
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,12 @@
 const express = require('express')
-const { check, validationResult } = require('express-validator');
+const {
+    check,
+    validationResult
+} = require('express-validator');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../Models/User')
+const secret = require('../config/default.json').secret
 
 
 const router = express.Router()
@@ -29,37 +34,44 @@ router.post('/',
         check('password', 'Please enter a password with 6 or more characters').isLength({
             min: 6
         }),
-    ], 
+    ],
     async (req, res) => {
 
         const errors = validationResult(req)
-            
-        if(!errors.isEmpty()){
+
+        if (!errors.isEmpty()) {
             return res.status(400).send(errors.array())
         }
 
         const { email, name, password } = req.body
 
-        const foundUser = await User.findOne({email})
-        
-        if(foundUser){
-            return res.status(400).send({err:'User already exist!'})            
+        const foundUser = await User.findOne({
+            email
+        })
+
+        if (foundUser) {
+            return res.status(400).send({
+                err: 'User already exist!'
+            })
         }
 
         const salt = await bcrypt.genSalt()
+
         const hash = await bcrypt.hash(password, salt)
 
         const user = new User({
             name,
             password: hash,
             email
-        });
+        })
 
-        user.save()
-            .then(result => res.status(200).send(result))
-            .catch(err => res.status(500).send({
-                err: "Cannot save user to database!"
-            }))
-})
+        const savedUser = await user.save()
+
+        const token = jwt.sign({id: savedUser._id}, secret, {
+            expiresIn: '1h'
+        })
+
+        return res.status(200).send({token})
+    })
 
 module.exports = router
